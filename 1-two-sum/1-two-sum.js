77df11c8eb05667bcb5e4c8d8c5e85cf29121c4f
Joel Hench <joelhench@gmail.com>
2022-06-16 22:11:16 -0700

    Time: 85 ms (78.48%), Space: 43.7 MB (19.11%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -4,17 +4,29 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-  for (let i = 0; i < nums.length; i++) {
-      for (let j = 0; j < nums.length; j++) {
-          let number1 = nums[i]
-          let number2 = nums[j]
-          if (number1 + number2 === target) {
-              if (i != j) {
-                return [i, j]    
-              }
+    let map = new Map();
+    
+    for(let i = 0; i < nums.length; i ++) {
+        if(map.has(target - nums[i])) {
+            return [map.get(target - nums[i]), i];
+        } else {
+            map.set(nums[i], i);
+        }
+    }
+	return [];
+};
+// var twoSum = function(nums, target) {
+//   for (let i = 0; i < nums.length; i++) {
+//       for (let j = 0; j < nums.length; j++) {
+//           let number1 = nums[i]
+//           let number2 = nums[j]
+//           if (number1 + number2 === target) {
+//               if (i != j) {
+//                 return [i, j]    
+//               }
               
            
-          }
-      }
-  }
-};
+//           }
+//       }
+//   }
+// };
