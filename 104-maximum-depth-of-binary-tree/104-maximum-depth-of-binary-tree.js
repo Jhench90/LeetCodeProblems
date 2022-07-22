273c7e40d248fa300e348b1454717a57ecf44b95
Joel Hench <joelhench@gmail.com>
2022-07-22 16:27:46 -0700

    Time: 84 ms (76.25%), Space: 45.6 MB (23.12%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
@@ -11,6 +11,5 @@
  * @return {number}
  */
 var maxDepth = function(root) {
-    if (root === null) return 0
-    return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1
+    return root ? Math.max(maxDepth(root.left), maxDepth(root.right)) + 1 : 0
 };
