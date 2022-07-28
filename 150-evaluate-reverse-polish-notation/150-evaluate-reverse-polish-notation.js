2e4314248a3a235d07f4ca296037043ee198c860
Joel Hench <joelhench@gmail.com>
2022-07-28 11:21:52 -0700

    Time: 80 ms (87.19%), Space: 44.7 MB (70.77%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.js
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.js
@@ -2,49 +2,65 @@
  * @param {string[]} tokens
  * @return {number}
  */
-var evalRPN = function(tokens) {
-    // while (tokens.length > 1) {
-        for (let i = 0; i < tokens.length; i++) {
-            // console.log(tokens)
-            let twoBack = Number(tokens[i-2])
-            let oneBack = Number(tokens[i-1])
-            let current = tokens[i]
-            if (current === '+' || current === '-' || current === '*' || current === '/') {
-                console.log(tokens)
-                if (current === '+') {
-                    let sum = twoBack + oneBack
-                    tokens.splice(i-2, 3, sum)
-                }
-                if (current === '-') {
-                    let diff = twoBack - oneBack
-                    tokens.splice(i-2, 3, diff)
-                }
-                if (current === '*') {
-                    let times = twoBack * oneBack
-                    tokens.splice(i-2, 3, times)
-                }
-                if (current === '/') {
-                    let remainder = twoBack / oneBack
-                    
-                    if (Math.floor(remainder) !== remainder) {
-                        remainder = remainder.toString()
-                        let cut = remainder.indexOf('.')
-                        remainder = remainder.slice(0, cut+1)
-                        console.log('remainder: ', remainder)
-                        remainder = Number(remainder)
-                    }
-                    
-                    
-                    
-                    // console.log('newRemainder: ', remainder)
-                    tokens.splice(i-2, 3, remainder)
-                }
-                i-=2
-            }
+var evalRPN = function (tokens) {
+    let stack = []
+    let operations = {
+        '+': (a,b)=>a+b,
+        '-': (a,b)=>a-b,
+        '*': (a,b)=>a*b,
+        '/': (a,b)=> a/b >= 0 ? Math.floor(a/b) : Math.ceil(a/b) 
+    }
+    
+    for (let i = 0; i < tokens.length; i++) {
+        let token = tokens[i]
+        if (operations[token]) {
+            let num1 = Number(stack.pop())
+            let num2 = Number(stack.pop())
+            let result = operations[token](num2, num1)
+            stack.push(result)
+        } else {
+            stack.push(token)
         }
-    return Math.floor(tokens[0])
-    // }
-};
+    }
+    return stack[0]
+}
+
+
+
+// var evalRPN = function(tokens) {
+//         for (let i = 0; i < tokens.length; i++) {
+//             let twoBack = Number(tokens[i-2])
+//             let oneBack = Number(tokens[i-1])
+//             let current = tokens[i]
+//             if (current === '+' || current === '-' || current === '*' || current === '/') {
+//                 if (current === '+') {
+//                     let sum = twoBack + oneBack
+//                     tokens.splice(i-2, 3, sum)
+//                 }
+//                 if (current === '-') {
+//                     let diff = twoBack - oneBack
+//                     tokens.splice(i-2, 3, diff)
+//                 }
+//                 if (current === '*') {
+//                     let times = twoBack * oneBack
+//                     tokens.splice(i-2, 3, times)
+//                 }
+//                 if (current === '/') {
+//                     let remainder = twoBack / oneBack
+                    
+//                     if (Math.floor(remainder) !== remainder) {
+//                         remainder = remainder.toString()
+//                         let cut = remainder.indexOf('.')
+//                         remainder = remainder.slice(0, cut+1)
+//                         remainder = Number(remainder)
+//                     }
+//                     tokens.splice(i-2, 3, remainder)
+//                 }
+//                 i-=2
+//             }
+//         }
+//     return Math.floor(tokens[0])
+// };
 // console.log('9: ', evalRPN(["2","1","+","3","*"])) //9
 // console.log('6: ', evalRPN(["4","13","5","/","+"])) //6
 // console.log('22: ', evalRPN(["10","6","9","3","+","-11","*","/","*","17","+","5","+"]))
