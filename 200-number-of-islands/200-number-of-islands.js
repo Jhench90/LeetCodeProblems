1d53271bf878661c9a5e1af1ea87cfd733024a22
Joel Hench <joelhench@gmail.com>
2022-08-03 11:27:57 -0700

    Time: 134 ms (49.69%), Space: 45.3 MB (54.02%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.js
+++ 200-number-of-islands/200-number-of-islands.js
@@ -1,5 +1,6 @@
 var numIslands = function(grid) {
     let islands = 0
+    
     for (let i = 0; i < grid.length; i++) {
         for (let j = 0; j < grid[0].length; j++) {
             if (grid[i][j] === '0') continue
@@ -7,23 +8,14 @@ var numIslands = function(grid) {
             clear(i, j)
         }
     }
-    function clear(i, j) {
+    function clear (i, j) {
         if (grid[i][j] === '0') return
         grid[i][j] = '0'
-        // console.log(islands)
-        if (i - 1 >= 0) {
-             clear(i-1, j)    
-        } 
-         if (i + 1 < grid.length) {
-             clear(i+1, j)
-        } 
-        if (j - 1 >= 0) {
-            clear(i, j-1)
-        }
-        if (j + 1 < grid[0].length) {
-             clear(i, j+1)   
-        }
+        i - 1 >= 0 ? clear(i-1, j) : null
+        j - 1 >= 0 ? clear(i, j-1): null
+        i + 1 < grid.length ? clear(i+1,j) : null 
+        j + 1 < grid[0].length ? clear(i, j+1): null
     }
-    
     return islands
+    
 };
