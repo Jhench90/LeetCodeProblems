ffcdb7533037ab5bae060c40d968a80af718e6b6
Joel Hench <joelhench@gmail.com>
2022-08-10 10:29:22 -0700

    Time: 2183 ms (5.26%), Space: 48.9 MB (7.89%) - LeetHub

---
+++ 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.js
@@ -0,0 +1,35 @@
+var maxSumTwoNoOverlap = function(nums, firstLen, secondLen) {
+    let firstMaxes = []
+    let secondMaxes = []
+    let sum = 0
+    let sum2 = 0
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i]
+        sum2 += nums[i]
+        if (i < firstLen - 1) {
+            firstMaxes.push(0)
+        }
+        if (i >= firstLen - 1) {
+            firstMaxes.push(sum)
+            sum-=nums[i-firstLen+1]
+        }
+        //-----//
+        if (i < secondLen - 1) {
+            secondMaxes.push(0)
+        }
+        if (i >= secondLen - 1) {
+            secondMaxes.push(sum2)
+            sum2-=nums[i-secondLen+1]
+        }
+    }
+    let max = -Infinity
+    for (let i = 0; i < nums.length; i++) {
+        let max1 = firstMaxes[i]
+        let max2 = secondMaxes.slice(0, i-firstLen + 1).concat(secondMaxes.slice(i+secondLen)).sort((a,b)=>b-a)[0]
+        // console.log(max1)
+        // console.log(secondMaxes.slice(0, i-firstLen).concat(secondMaxes.slice(i+secondLen)))
+        
+        max = Math.max(max, max1 + max2)
+    }
+    return max
+};
