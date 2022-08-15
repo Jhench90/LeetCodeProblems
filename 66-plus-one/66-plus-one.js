87f377fe87d583986cc1b9122de39085e101e38a
Joel Hench <joelhench@gmail.com>
2022-08-15 10:34:24 -0700

    Time: 69 ms (83.87%), Space: 42.4 MB (7.48%) - LeetHub

---
+++ 66-plus-one/66-plus-one.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+    let carry = 0
+    let leftover = 0
+    let i = digits.length - 1 //0
+    let sum = digits[i] + 1 //10
+    
+    if (sum >= 10) {
+        leftover = sum - 10 //0
+        carry = 1 //1
+        carryHelp()
+    } else {
+        digits[i] = sum;
+    }
+    
+    function carryHelp() {
+        digits[i] = leftover //[0]
+        i-- //-1
+        if (i < 0) { 
+            sum = carry //1
+        } else {
+            sum = digits[i] + carry   
+        }
+        if (sum >= 10) {
+            leftover = sum - 10
+            carry = 1
+            carryHelp()
+        } else {
+            if (i < 0) {
+                digits.unshift(sum) //[1,0]
+            } else {
+                digits[i] = sum;            
+            }
+        }   
+    }    
+    return digits
+};
