288dd61dc329d970392e2f04ab03182a16fee687
Joel Hench <joelhench@gmail.com>
2022-08-16 12:43:48 -0700

    Time: 300 ms (5.20%), Space: 54.7 MB (5.16%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.java
@@ -0,0 +1,46 @@
+class Solution {
+    public boolean isPalindrome(int num) {
+      ArrayList<Integer> destructured = new ArrayList<Integer>();
+        if (num < 0) {
+            return false;
+        }
+        if (num == 0) {
+            return true;
+        }
+        int original = num;
+        int remainder = 0;
+        while (num != 0) {
+            remainder = num % 10;
+            destructured.add(0, remainder);
+            num /= 10;
+        }
+        // 121 % 10 = 1
+        // 121 /= 10 = 12
+        // 12 % 10 = 2
+        // 12 / 10 = 1
+        System.out.println("answer is: " + destructured);
+        System.out.println("destructured size: " + destructured.size()/2);
+        int i = 0;
+        while (i <= destructured.size()/2) {
+            System.out.println("i is: " + i);   
+            if (destructured.get(i) != destructured.get(destructured.size() - i - 1)) {
+                   return false;
+               }
+            i++;
+        }            
+        return true;
+    }
+}
+
+// class Solution {
+//     public boolean isPalindrome(int num) {
+//         if(num < 0) return false;
+//         int reversed = 0, remainder, original = num;
+//         while(num !=0) {
+//             remainder = num % 10;
+//             reversed = reversed * 10 + remainder;
+//             num /= 10;
+//         }
+//         return original == reversed;
+//     }
+// }
