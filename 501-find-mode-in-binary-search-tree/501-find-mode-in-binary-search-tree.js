0293b366a000050739b543a353e631396236e7e5
Joel Hench <joelhench@gmail.com>
2022-08-25 10:16:47 -0700

    Time: 72 ms (99.07%), Space: 47.9 MB (95.02%) - LeetHub

--- 501-find-mode-in-binary-search-tree/501-find-mode-in-binary-search-tree.js
+++ 501-find-mode-in-binary-search-tree/501-find-mode-in-binary-search-tree.js
@@ -1,18 +1,7 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-/**
- * @param {TreeNode} root
- * @return {number[]}
- */
 var findMode = function(node) {
+    //edge cases
     if (node === null) return
-    if (node.left === null && node.right === null) return [node.val]
+    // if (node.left === null && node.right === null) return [node.val]
     
     let mode = []
     let current = undefined
@@ -29,7 +18,6 @@ var findMode = function(node) {
             current = node.val
             count = 1
         }
-        
         if (count === max) {
             mode.push(node.val)
         }
