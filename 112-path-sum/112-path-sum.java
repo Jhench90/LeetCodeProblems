238498372e5dcf083de96c9dec70734a92f7d3b5
Joel Hench <joelhench@gmail.com>
2022-08-24 17:11:38 -0700

    Time: 0 ms (100.00%), Space: 41.9 MB (98.24%) - LeetHub

--- 112-path-sum/112-path-sum.java
+++ 112-path-sum/112-path-sum.java
@@ -15,22 +15,21 @@
  */
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        if (root == null) 
-            return false;        
+        if (root == null) return false;
         
         targetSum -= root.val;
-        if ((root.left==null) && (root.right==null))
-            return (targetSum==0);
-        return hasPathSum(root.left, targetSum) || hasPathSum(root.right, targetSum);
         
+        if ((root.left == null) && (root.right == null)) {
+            return (targetSum == 0);
+        }
+        
+        return hasPathSum(root.left, targetSum) || hasPathSum(root.right, targetSum);     
     }
 }
-
 /*
-declare a sum variable
-iterate thru tree nodes using DFS
-add val to sum
-if reach leaf node (no right or left)
-if sum equals target
-return true
+check if root is null, return if so
+subtract val from targetSum
+if root left and root right are both null
+return if targetSum = 0
+return recursively searching left or right subtrees returning true
 */
