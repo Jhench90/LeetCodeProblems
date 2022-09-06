f984eb92024bede737c4d13ea35086adb2acdb5d
Joel Hench <joelhench@gmail.com>
2022-09-05 21:33:45 -0700

    Time: 8 ms (67.91%), Space: 42.3 MB (64.85%) - LeetHub

--- 163-missing-ranges/163-missing-ranges.java
+++ 163-missing-ranges/163-missing-ranges.java
@@ -1,22 +1,23 @@
 class Solution {
     public List<String> findMissingRanges(int[] nums, int lower, int upper) {
         
-        
-        
         //declare answer array
         List<String> answer = new ArrayList<>();
         
+        //edge case
         if (nums.length == 0) { 
             answer.add(helper(lower, upper));
             return answer; 
         }
         
+        //check lower bound against first value of array
         int left = lower;
         int right = nums[0] - 1;
         if (left <= right) {
             answer.add(helper(left, right));
         }
         
+        //check the array
         for (int i = 0; i < nums.length - 1; i++) {
             left = nums[i] + 1;
             right = nums[i+1] - 1;
@@ -25,23 +26,22 @@ class Solution {
             }
         }
         
+        //check last val of array against upper bound
         left = nums[nums.length - 1] + 1;
         right = upper;
         if (left <= right) {
             answer.add(helper(left, right));
         }
         
+        //return the answer
         return answer;
-        
     }
+    
+    //helper function to return the string format
     private String helper(int left, int right) {
         if (left == right) {
             return String.valueOf(left);
         }
         return left + "->" + right;
-    }
-    //helper
-        //if right less than left, dont push
-        //if left equal to right, only push a single number
-        //if left less than right, make a string range and push it   
+    }  
 }
