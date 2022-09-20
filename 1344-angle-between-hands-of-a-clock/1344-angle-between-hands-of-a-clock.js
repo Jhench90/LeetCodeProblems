e65e229f2201ac3e18d2cdbb8427d425c3d759e5
Joel Hench <joelhench@gmail.com>
2022-09-20 15:53:39 -0700

    Time: 119 ms (5.60%), Space: 41.7 MB (89.60%) - LeetHub

---
+++ 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number} hour
+ * @param {number} minutes
+ * @return {number}
+ */
+var angleClock = function(hour, minutes) {
+    
+    hour = hour === 12 ? 0 : hour
+    let minuteRatio = minutes / 60;
+    let minutesDegrees = minuteRatio * 360
+    
+    let hoursDegrees = hour * 30 + minutesDegrees / 12
+    
+    let distance = Math.min(Math.abs(minutesDegrees - hoursDegrees), 
+                           Math.abs(hoursDegrees - minutesDegrees))
+    return distance > 180 ? 360 - distance : distance
+};
+// console.log('expect 76.5, actual: ', angleClock(1, 57))
+
+/*
+Algorithm
+calculate distance of minute hand from 0 position as ratio of 1.0
+calculate distance of hour hand from 0 position as ratio of 1.0 + minute hand ratio * 30
+
+calculate first - second math abs
+calculate second - first math abs
+
+take the lesser of the two angles calculated
+
+multiply it by 360 and return the number
+*/
