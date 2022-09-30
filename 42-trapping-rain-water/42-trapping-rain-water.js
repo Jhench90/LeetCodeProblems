221c5f90baf6a01c07062d7fd18b27cfae06e031
Joel Hench <joelhench@gmail.com>
2022-09-30 12:23:39 -0700

    Time: 248 ms (5.12%), Space: 46.5 MB (16.82%) - LeetHub

---
+++ 42-trapping-rain-water/42-trapping-rain-water.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function(height) {
+    let leftMaxes = [];
+    let rightMaxes = [];
+    let max = -Infinity;
+    let trappedWater = 0;
+    
+    const findMaxes = (array, value, id) => {
+         if (value > max) { max = value; }
+         if (id === 1) { array.push(max); }
+         if (id === 2) { array.unshift(max); }
+    }
+    
+    for (let i = 0; i < height.length; i++) {
+        findMaxes(leftMaxes, height[i], 1);        
+    }
+    max = -Infinity;
+    for (let i = height.length - 1; i >= 0; i--) {
+        findMaxes(rightMaxes, height[i], 2);
+    };
+    
+    for (let i = 0; i < leftMaxes.length; i++) {
+        let column = Math.min(leftMaxes[i], rightMaxes[i]) - height[i];
+        trappedWater += column;
+    }
+    return trappedWater;
+}
