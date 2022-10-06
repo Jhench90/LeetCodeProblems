cf7c2692d3dc2d4de70e3af5cefb6e5a618ec35f
Joel Hench <joelhench@gmail.com>
2022-10-06 15:34:01 -0700

    Time: 4 ms (46.75%), Space: 48 MB (37.81%) - LeetHub

---
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode();
+        ListNode node = head;
+        int carry = 0;
+        
+        while (l1 != null || l2 != null || carry != 0) {
+            int x = (l1 != null) ? l1.val : 0;
+            int y = (l2 != null) ? l2.val : 0;
+            int sum = x + y + carry;
+            carry = sum / 10;
+            node.next = new ListNode(sum % 10);
+            node = node.next;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        return head.next;
+    }
+}
+
+/*
+algorithm
+
+use a while loop to add each set of numbers together in the list
+if the sum is greater than 10, use a carry variable to add the carry to the next iteration
+if either list ends, set the value to add equal to 0
+if both lists are ended, and carry is ended, end the loop
+
+*/
